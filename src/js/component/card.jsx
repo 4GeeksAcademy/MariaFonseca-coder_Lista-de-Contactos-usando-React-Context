cd0f80cd2fcf5a34c4272ca1a47a359b304a7c60
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-19 02:33:15 +0000

    Base de cards listas

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
+import muchacha1 from "../../img/muchacha1.jpg";
 
 export const Card = () => {
     const { store, actions } = useContext(Context);
@@ -12,18 +13,30 @@ export const Card = () => {
         <div>
             {store.contacts && store.contacts.length > 0 ? (
                 store.contacts.map((contact, index) => (
-                    <div className="card mb-3" style={{ maxWidth: "540px" }} key={index}>
-                        <div className="row g-0">
+                    <div className="card mb-1" style={{ width: "1000px", height: "200px", margin: "auto" }} key={index}>
+                        <div className="row">
                             <div className="col-md-4">
-                                <img src="https://via.placeholder.com/150" className="img-fluid rounded-start" alt="Contacto" />
+                                <img
+                                    src={`https://picsum.photos/300/188?random=${index + 1}`}
+                                    className="img-fluid rounded-start" style={{ width: "100%", objectFit: "cover" }}
+                                    alt="Contacto"
+                                />
                             </div>
                             <div className="col-md-8">
-                                <div className="card-body">
+                                <div className="card-body text-start">
                                     <h5 className="card-title">{contact.full_name}</h5>
                                     <p className="card-text">Email: {contact.email}</p>
                                     <p className="card-text">Teléfono: {contact.phone}</p>
                                     <p className="card-text">Dirección: {contact.address}</p>
-                                    <p className="card-text"><small className="text-muted">Última actualización hace 3 minutos</small></p>
+                                    <p className="card-text"><small className="text-muted"></small></p>
+                                </div>
+                                <div className="position-absolute top-0 end-0 p-2">
+                                    <button className="btn btn-outline-primary me-2">
+                                        <i className="fa-regular fa-pen-to-square"></i>
+                                    </button>
+                                    <button className="btn btn-outline-danger">
+                                        <i className="fa-regular fa-trash-can"></i>
+                                    </button>
                                 </div>
                             </div>
                         </div>
