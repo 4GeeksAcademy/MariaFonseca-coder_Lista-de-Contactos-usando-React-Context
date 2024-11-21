f49b8b199549a3382c54d4f81e72c1d1b9035f54
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-21 06:44:38 +0000

    Diseño de botones mejorado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -29,26 +29,58 @@ export const Navbar = () => {
 					</h1>
 				</Link>
 
-				<div style={{ marginLeft: "auto", display: "flex", alignItems: "center" }}>
-					<button
-						className="btn btn-success"
-						style={{ fontFamily: "'Sour Gummy', sans-serif", marginRight: "15px" }}
-						onClick={handleCreateAgenda}
-					>
-						Create Agenda
-					</button>
+				{/* Botón hamburguesa */}
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="offcanvas"
+					data-bs-target="#offcanvasNavbar"
+					aria-controls="offcanvasNavbar"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
 
-					<Link to="/createContact" className="text-decoration-none">
+				{/* Offcanvas para el menú hamburguesa */}
+				<div
+					className="offcanvas offcanvas-end"
+					tabIndex="-1"
+					id="offcanvasNavbar"
+					aria-labelledby="offcanvasNavbarLabel"
+				>
+					<div className="offcanvas-header">
+						<h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+							Menu
+						</h5>
 						<button
-							className="btn btn-primary"
-							style={{ fontFamily: "'Sour Gummy', sans-serif", marginRight: "15px" }}
-						>
-							Add new contact
-						</button>
-					</Link>
+							type="button"
+							className="btn-close text-reset"
+							data-bs-dismiss="offcanvas"
+							aria-label="Close"
+						></button>
+					</div>
+					<div className="offcanvas-body">
+						<div className="d-flex flex-column gap-2">
+							<button
+								className="btn btn-success w-100"
+								style={{ fontFamily: "'Sour Gummy', sans-serif" }}
+								onClick={handleCreateAgenda}
+							>
+								Create Agenda
+							</button>
+							<Link to="/createContact" className="text-decoration-none">
+								<button
+									className="btn btn-primary w-100"
+									style={{ fontFamily: "'Sour Gummy', sans-serif" }}
+								>
+									Add contact
+								</button>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</nav>
 
+			{/* Alerts */}
 			{alertType === "success" && (
 				<div className="alert alert-success mt-2" role="alert">
 					MariaFonseca's agenda has been created successfully!
